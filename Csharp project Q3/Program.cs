56ebd5e9e75523fae6f0e7cfe81a0bdcd9915bd0
Abinet Tamiru <Abinettamiru@outlook.com>
2022-04-30 11:10:23 +0300

    Add project files.

---
+++ Csharp project Q3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_project_Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("the current date and time is: ");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+        }
+    }
+}
